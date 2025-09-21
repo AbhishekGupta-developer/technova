8d324becbe9de766e5f67843be439cdea0dae2c3
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-09-21 16:41:59 +0530

    Add custom JPQL query 'getStudentsByCourse' method

--- src/main/java/com/myorganisation/technova/repository/StudentRepository.java
+++ src/main/java/com/myorganisation/technova/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package com.myorganisation.technova.repository;
 
 import com.myorganisation.technova.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +15,8 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     // Custom finder methods
     Optional<Student> findByPhone(String phone);
     List<Student> findByNameAndCourse(String name, String course);
+
+    // Custom JPQL -> Java Persistence Query Language
+    @Query("SELECT s FROM Student s WHERE s.course = :course")
+    List<Student> getStudentsByCourse(@Param("course") String course);
 }
