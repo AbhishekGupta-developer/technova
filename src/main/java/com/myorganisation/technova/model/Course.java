d6b506e91139986376c2723ce2fc20ac3c01afd3
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-09-28 21:03:44 +0530

    Add course entity

---
+++ src/main/java/com/myorganisation/technova/model/Course.java
@@ -0,0 +1,17 @@
+package com.myorganisation.technova.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Double fee;
+    private Double duration;
+}
