795026b02a688dbdd982b3e90721c0c81044a519
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-10-05 15:35:34 +0530

    Add Account entity

---
+++ src/main/java/com/myorganisation/technova/model/Account.java
@@ -0,0 +1,16 @@
+package com.myorganisation.technova.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "accounts")
+@Data
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double total;
+    private Double due;
+}
