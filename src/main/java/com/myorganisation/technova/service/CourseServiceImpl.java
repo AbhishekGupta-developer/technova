e1eb3c3db13537f8243f542749b665213e20e93e
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-10-05 15:34:51 +0530

    Implement CourseService

--- src/main/java/com/myorganisation/technova/service/CourseServiceImpl.java
+++ src/main/java/com/myorganisation/technova/service/CourseServiceImpl.java
@@ -3,34 +3,104 @@ package com.myorganisation.technova.service;
 import com.myorganisation.technova.dto.request.CourseRequestDto;
 import com.myorganisation.technova.dto.response.CourseResponseDto;
 import com.myorganisation.technova.dto.response.GenericResponseDto;
+import com.myorganisation.technova.model.Course;
+import com.myorganisation.technova.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Service
 public class CourseServiceImpl implements CourseService {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
     @Override
     public CourseResponseDto addCourse(CourseRequestDto courseRequestDto) {
-        return null;
+        Course course = new Course();
+        course.setName(courseRequestDto.getName());
+        course.setFee(courseRequestDto.getFee());
+        course.setDuration(courseRequestDto.getDuration());
+
+        courseRepository.save(course);
+
+        CourseResponseDto courseResponseDto = new CourseResponseDto();
+        courseResponseDto.setId(course.getId());
+        courseResponseDto.setName(course.getName());
+        courseResponseDto.setFee(course.getFee());
+        courseResponseDto.setDuration(course.getDuration());
+
+        return courseResponseDto;
     }
 
     @Override
     public CourseResponseDto getCourse(Long id) {
-        return null;
+        Course course = courseRepository.findById(id).orElse(null);
+
+        CourseResponseDto courseResponseDto = new CourseResponseDto();
+        courseResponseDto.setId(course.getId());
+        courseResponseDto.setName(course.getName());
+        courseResponseDto.setFee(course.getFee());
+        courseResponseDto.setDuration(course.getDuration());
+
+        return courseResponseDto;
     }
 
     @Override
     public List<CourseResponseDto> getAllCourses() {
-        return List.of();
+        List<Course> courseList = courseRepository.findAll();
+        List<CourseResponseDto> courseResponseDtoList = new LinkedList<>();
+
+        for(Course course : courseList) {
+            CourseResponseDto courseResponseDto = new CourseResponseDto();
+            courseResponseDto.setId(course.getId());
+            courseResponseDto.setName(course.getName());
+            courseResponseDto.setFee(course.getFee());
+            courseResponseDto.setDuration(course.getDuration());
+
+            courseResponseDtoList.add(courseResponseDto);
+        }
+
+        return courseResponseDtoList;
     }
 
     @Override
     public CourseResponseDto updateCourse(Long id, CourseRequestDto courseRequestDto) {
-        return null;
+        Course course = courseRepository.findById(id).orElse(null);
+        course.setName(courseRequestDto.getName());
+        course.setFee(courseRequestDto.getFee());
+        course.setDuration(courseRequestDto.getDuration());
+
+        courseRepository.save(course);
+
+        CourseResponseDto courseResponseDto = new CourseResponseDto();
+        courseResponseDto.setId(course.getId());
+        courseResponseDto.setName(course.getName());
+        courseResponseDto.setFee(course.getFee());
+        courseResponseDto.setDuration(course.getDuration());
+
+        return courseResponseDto;
     }
 
     @Override
     public GenericResponseDto removeCourse(Long id) {
-        return null;
+        Course course = courseRepository.findById(id).orElse(null);
+        GenericResponseDto genericResponseDto = new GenericResponseDto();
+
+        if(course != null) {
+            String name = course.getName();
+            courseRepository.deleteById(id);
+
+            String message = "Course: " + name + "(" + id + ") has been removed";
+            genericResponseDto.setSuccess(true);
+            genericResponseDto.setMessage(message);
+        } else {
+            genericResponseDto.setSuccess(false);
+            genericResponseDto.setMessage("Course doesn't exist");
+        }
+
+        return genericResponseDto;
     }
 }
