e9a5c110abb297e06b09f03c9225c60a0dd00358
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-10-12 19:25:50 +0530

    Refactor request dto according to ManyToMany mapping

--- src/main/java/com/myorganisation/technova/dto/request/StudentRequestDto.java
+++ src/main/java/com/myorganisation/technova/dto/request/StudentRequestDto.java
@@ -2,9 +2,12 @@ package com.myorganisation.technova.dto.request;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class StudentRequestDto {
     private String name;
-    private String course;
+    private List<Long> courses = new ArrayList<>();
     private String phone;
 }
