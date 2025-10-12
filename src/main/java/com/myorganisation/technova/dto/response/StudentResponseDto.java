f3d9a6544532a4d6db9b59ba050147859ccbe446
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-10-12 19:26:17 +0530

    Refactor response dto according to ManyToMany mapping

--- src/main/java/com/myorganisation/technova/dto/response/StudentResponseDto.java
+++ src/main/java/com/myorganisation/technova/dto/response/StudentResponseDto.java
@@ -3,11 +3,13 @@ package com.myorganisation.technova.dto.response;
 import com.myorganisation.technova.model.Account;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class StudentResponseDto {
     private Long id;
     private String name;
-    private String course;
+    private List<CourseResponseDto> courses;
     private String phone;
     private Account account;
 }
