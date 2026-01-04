c8209e57ab463ca5169ba9aa89536ccce3e7d18b
Abhishek Gupta <abhishekg.developer@gmail.com>
2026-01-04 20:26:00 +0530

    Permit all '/api/users/**' API in security config

--- src/main/java/com/myorganisation/technova/config/SecurityConfig.java
+++ src/main/java/com/myorganisation/technova/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.myorganisation.technova.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,7 +16,8 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(
                 auth -> auth
-                        .requestMatchers("/").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/users/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/users/**").permitAll()
                         .anyRequest().authenticated()
         ).httpBasic(Customizer.withDefaults());
 
